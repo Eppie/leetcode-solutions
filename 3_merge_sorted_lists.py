29abc0fb8ea532fd016ae5928f1091dbb9ab96d7
Andrew Epstein <aepstein@udel.edu>
2024-05-03 13:33:06 -0400

    solve 3

---
+++ 3_merge_sorted_lists.py
@@ -0,0 +1,28 @@
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+
+class Solution:
+    def mergeTwoLists(
+        self, list1: ListNode | None, list2: ListNode | None
+    ) -> ListNode | None:
+        pre_head = ListNode()
+        current = pre_head
+        while list1 and list2:
+            if list1.val < list2.val:
+                current.next = list1
+                current = list1
+                list1 = list1.next
+            else:
+                current.next = list2
+                current = list2
+                list2 = list2.next
+
+        if list1:
+            current.next = list1
+        if list2:
+            current.next = list2
+
+        return pre_head.next
